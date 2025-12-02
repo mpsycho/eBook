8f2b734152cbb897e9cb9e32716d0eab309555dc
psycho715 <marios.psychogios@gmail.com>
2025-12-02 17:24:25 +0200

    initial commit, tailwindcss and header component

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: false,
+  templateUrl: './header.html',
+  styleUrls: ['./header.scss'],
+})
+export class Header {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+}
